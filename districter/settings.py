74537593bb6dbd3872e8c38dd4fbafb93ef2a9d1
Lauren Johnston <ljohnston@princeton.edu>
2019-03-29 14:04:15 -0400

    settings.py secret_key fix

--- districter/settings.py
+++ districter/settings.py
@@ -20,11 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = '-3jgv28fspzqc3djgfm!2q+f$vwwlcwc3%_59w70voi*^37kdh'
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-DEBUG_PROPAGATE_EXCEPTIONS = True
 
 ALLOWED_HOSTS = ['*']
 
