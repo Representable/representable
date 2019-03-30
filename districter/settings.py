77bb6c45a23a4d4074b36bb60924b4176dccc640
Theodor Marcu <8714338+theodormarcu@users.noreply.github.com>
2019-03-30 13:26:45 -0400

    Added mapbox with polygons

--- districter/settings.py
+++ districter/settings.py
@@ -22,7 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
+
+DEBUG_PROPAGATE_EXCEPTIONS = True
 
 ALLOWED_HOSTS = ['*']
 
