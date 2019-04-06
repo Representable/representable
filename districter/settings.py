0da0b45194c7e272f125833708aee3b2f5062f93
Lauren Johnston <ljohnston@princeton.edu>
2019-04-06 10:21:35 -0400

    debug = True, temporary

--- districter/settings.py
+++ districter/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
-DEBUG = False
+DEBUG = True
 DEBUG_PROPAGATE_EXCEPTIONS = True
 
 ALLOWED_HOSTS = ['*']
