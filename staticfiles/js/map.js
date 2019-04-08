f307bfe03173b28d5d468556025c91a9c98d6d4d
Kyle Barnes <ktbarnes@princeton.edu>
2019-04-08 00:03:54 -0400

    some map display stuff

--- staticfiles/js/map.js
+++ staticfiles/js/map.js
@@ -1,16 +1,16 @@
 /* JS file from mapbox site -- display a polygon */
 /* https://docs.mapbox.com/mapbox-gl-js/example/geojson-polygon/ */
 var map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/mapbox/dark-v10',
-  center: [-68.13734351262877, 45.137451890638886],
-  zoom: 5
+  container: 'map', // container id
+  style: 'mapbox://styles/mapbox/dark-v10', //mapbox style -- dark is pretty for data visualization :o)
+  center: [-74.65545, 40.341701], // starting position - Princeton, NJ :)
+  zoom: 9 // starting zoom -- higher is closer
 });
 
-map.addControl(new mapboxgl.NavigationControl());
+map.addControl(new mapboxgl.NavigationControl()); // plus minus top right corner
 
 map.on('load', function () {
-
+  // the stuff that happens when the map is loaded...
   /*
   var request = new XMLhttpRequest();
   request.open("GET", "../../assets/NJBlocks.json", false);
@@ -33,8 +33,10 @@ map.addLayer({
 },
 });
 */
+
+//it's just... a square...
 map.addLayer({
-  'id': 'maine',
+  'id': 'dummy',
   'type': 'fill',
   'source': {
     'type': 'geojson',
@@ -42,26 +44,10 @@ map.addLayer({
       'type': 'Feature',
       'geometry': {
         'type': 'Polygon',
-        'coordinates': [[[-67.13734351262877, 45.137451890638886],
-        [-66.96466, 44.8097],
-        [-68.03252, 44.3252],
-        [-69.06, 43.98],
-        [-70.11617, 43.68405],
-        [-70.64573401557249, 43.090083319667144],
-        [-70.75102474636725, 43.08003225358635],
-        [-70.79761105007827, 43.21973948828747],
-        [-70.98176001655037, 43.36789581966826],
-        [-70.94416541205806, 43.46633942318431],
-        [-71.08482, 45.3052400000002],
-        [-70.6600225491012, 45.46022288673396],
-        [-70.30495378282376, 45.914794623389355],
-        [-70.00014034695016, 46.69317088478567],
-        [-69.23708614772835, 47.44777598732787],
-        [-68.90478084987546, 47.184794623394396],
-        [-68.23430497910454, 47.35462921812177],
-        [-67.79035274928509, 47.066248887716995],
-        [-67.79141211614706, 45.702585354182816],
-        [-67.13734351262877, 45.137451890638886]]]
+        'coordinates': [[[-74.0, 40.0],
+        [-74.0, 40.5],
+        [-74.5, 40.5],
+        [-74.5, 40.0]]]
       }
     }
   },
@@ -73,6 +59,7 @@ map.addLayer({
 });
 });
 
+//create a button ! toggles layers based on their IDs
 var toggleableLayerIds = [ 'Census Blocks', 'Districts' ];
 
 for (var i = 0; i < toggleableLayerIds.length; i++) {
