6dc9b4816d797b88441ea1f2397ed6d8d9abf90f
Lauren Johnston <ljohnston@princeton.edu>
2019-04-08 14:28:59 -0400

    bootstrap override templates for allauth

--- districter/urls.py
+++ districter/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('main.urls')),
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
+    #path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('allauth.urls')),
     path('select2/', include('django_select2.urls')),
 ]
