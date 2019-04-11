b165f8307d5bacffea30926780e98c6e59f0f8d1
Theodor Marcu <8714338+theodormarcu@users.noreply.github.com>
2019-04-11 19:57:17 -0400

    Updated the models.

--- main/models.py
+++ main/models.py
@@ -19,18 +19,6 @@ class User(AbstractUser):
 - This is my community / I am creating this on behalf of another community
 '''
 
-# 
-# class Migration(migrations.Migration):
-#
-#     dependencies = [
-#         ('user', '0001_initial'),
-#     ]
-#
-#     operations = [
-#         migrations.RenameModel('User', 'main.User')
-#     ]
-#
-
 # https://www.census.gov/topics/population/race/about.html
 RACE_CHOICES = (
     ('white', 'White'),
@@ -49,29 +37,6 @@ RACE_CHOICES = (
     ),
     ('other', 'Other'),
 )
-
-# class Entry(models.Model):
-#     # Max Length = 100 chars, Blank=False - Field cannot be false. Unique - field has to be unique.
-#     entry_ID = models.CharField(max_length=100, blank=False, unique=True, default=uuid.uuid4)
-#     # Store the location searched by the user (lat-long)
-#     entry_location = models.PointField()
-#     # Store the polygon created by the user.
-#     entry_polygon = models.PolygonField(serialize=True)
-#     # Foreign Key = User (Many to One   )
-#     # https://docs.djangoproject.com/en/2.2/topics/db/examples/many_to_one/
-#     creator_ID =  models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-#
-#     def __str__(self):
-#         return str(self.entry_polygon)
-#     class Meta:
-#         db_table = "main_entry"
-#
-# class Community(models.Model):
-#     zipcode = models.CharField(max_length=5)
-#     race = ArrayField(models.CharField(max_length=50,choices=RACE_CHOICES),default=list,blank=False)
-#     #race = models.ManyToManyField(Race)
-#     issues =  models.CharField(max_length=100)
-#     is_my_community = models.BooleanField()
 
 
 class CommunityEntry(models.Model):
