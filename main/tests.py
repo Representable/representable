75f2c87786805ed0e818e31ab282321ee102ce17
Theodor Marcu <8714338+theodormarcu@users.noreply.github.com>
2019-04-27 13:57:33 -0400

    Updated Travis YML.

--- main/tests.py
+++ main/tests.py
@@ -62,39 +62,39 @@ class UserTest(TestCase):
         self.user.delete()
 
 
-# class CommunityEntryTest(TestCase):
-#     '''
-#     Unit tests for community entry.
-#     Includes tests for:
-#         - Form
-#         - Formset
-#         - Model
-#     '''
-#     def setUp(self):
-#         '''
-#         This function is called before every test.
-#         Create and login a fake user.
-#         '''
-#         # Create a fake user.
-#         self.user = get_user_model().objects.create_user('johndoe', 'john@doe.com', 'johndoe')
-#         # Fake user login
-#         self.client.login(username='johndoe', password='johndoe')
-#
-#     def tearDown(self):
-#         '''
-#         This function runs after every test. It deletes the fake user.
-#         '''
-#         self.client.logout()
-#         self.user.delete()
-#
-#
-#
-#     def test_valid_form(self):
-#         '''
-#         Test that the server returns the right form when /entry/ is requested.
-#         '''
-#         c = Client()
-#         response = c.get('/entry/')
-#         form = CommunityForm(initial=EntryView.get_initial())
-#         self.assertEqual(response.context['form'], form)
-#         self.assertEqual(response.context[mapbox_key], os.environ.get('DISTR_MAPBOX_KEY'))
+class CommunityEntryTest(TestCase):
+    '''
+    Unit tests for community entry.
+    Includes tests for:
+        - Form
+        - Formset
+        - Model
+    '''
+    def setUp(self):
+        '''
+        This function is called before every test.
+        Create and login a fake user.
+        '''
+        # Create a fake user.
+        self.user = get_user_model().objects.create_user('johndoe', 'john@doe.com', 'johndoe')
+        # Fake user login
+        self.client.login(username='johndoe', password='johndoe')
+
+    def tearDown(self):
+        '''
+        This function runs after every test. It deletes the fake user.
+        '''
+        self.client.logout()
+        self.user.delete()
+
+
+
+    def test_valid_form(self):
+        '''
+        Test that the server returns the right form when /entry/ is requested.
+        '''
+        c = Client()
+        response = c.get('/entry/')
+        form = CommunityForm(initial=EntryView.get_initial())
+        self.assertEqual(response.context['form'], form)
+        self.assertEqual(response.context[mapbox_key], os.environ.get('DISTR_MAPBOX_KEY'))
