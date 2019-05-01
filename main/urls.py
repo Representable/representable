92ecaac8a5bb23d0274297e32093477457604a8e
Preeti Iyer <preetiiyer@nat-oitwireless-inside-vapornet100-10-9-77-85.princeton.edu>
2019-05-01 03:29:31 -0400

    typos

--- main/urls.py
+++ main/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('thanks/', views.Thanks.as_view(), name='thanks'),
     path('entry/', views.EntryView.as_view(), name='entry'),
     path('main/', views.MainView.as_view(), name='main_test'),
-    path('about/', views.About.as_view(), name='about'),
+    path('about/', views.About.as_view(), name='MeetTheTeam'),
     path('review/', views.Review.as_view(), name='review')
 ]
