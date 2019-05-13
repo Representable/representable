689491b26d02e3b1185fb6921a352e9edf7fc9ed
Lauren Johnston <ljohnston@princeton.edu>
2019-05-12 21:49:03 -0400

    clean up folders

--- main/static/js/main.js
+++
@@ -1,6 +0,0 @@
-{% load static %}
-
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', "{% static assets/particles.json' %}", function() {
-  console.log('callback - particles.min.js config loaded');
-});
