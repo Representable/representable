c714bb1f6feaf24d1eeb60cb3071fce08769253a
Theodor Marcu <8714338+theodormarcu@users.noreply.github.com>
2019-06-10 09:29:22 -0700

    Added license headers to source code.

--- main/static/main/js/wicket.js
+++ main/static/main/js/wicket.js
@@ -1,22 +1,21 @@
-/** @license
+/*
+ * Copyright (c) 2019- Representable Team (Theodor Marcu, Lauren Johnston, Somya Arora, Kyle Barnes, Preeti Iyer).
  *
- *  Copyright (C) 2012 K. Arthur Endsley (kaendsle@mtu.edu)
- *  Michigan Tech Research Institute (MTRI)
- *  3600 Green Court, Suite 100, Ann Arbor, MI, 48105
+ * This file is part of Representable 
+ * (see http://representable.org).
  *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 (function (root, factory) {
