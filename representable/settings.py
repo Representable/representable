c1ce2dcc230b24e647cf417360e38666e165a3a6
Theodor Marcu <marcu.theodor@gmail.com>
2019-11-28 02:13:54 -0500

    Added SSL settings.

--- representable/settings.py
+++ representable/settings.py
@@ -96,6 +96,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+# https://help.heroku.com/J2R1S4T8/can-heroku-force-an-application-to-use-ssl-tls
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+
 ROOT_URLCONF = 'representable.urls'
 
 TEMPLATES = [
