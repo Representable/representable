e101897f04736d18eef57ba0ada83482b46ffa0b
Andrew Milich <milichab@gmail.com>
2019-12-17 20:44:16 -0500

    re-adding Lauren's precommit hooks

--- main/migrations/0002_communityentry_admin_approved.py
+++ main/migrations/0002_communityentry_admin_approved.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('main', '0001_initial'),
+        ("main", "0001_initial"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='communityentry',
-            name='admin_approved',
+            model_name="communityentry",
+            name="admin_approved",
             field=models.BooleanField(default=False),
         ),
     ]
--- representable/wsgi.py
+++ representable/wsgi.py
@@ -1,7 +1,7 @@
 #
 # Copyright (c) 2019- Representable Team (Theodor Marcu, Lauren Johnston, Somya Arora, Kyle Barnes, Preeti Iyer).
 #
-# This file is part of Representable 
+# This file is part of Representable
 # (see http://representable.org).
 #
 # This program is free software: you can redistribute it and/or modify
@@ -30,6 +30,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'representable.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "representable.settings")
 
 application = get_wsgi_application()
