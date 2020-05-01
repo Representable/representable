14dcbd43592f70fc7c34bba62cdd361ec34796fe
Theodor Marcu <marcu.theodor@gmail.com>
2020-05-01 13:50:16 -0400

    new backend

--- representable/settings/dev.py
+++ representable/settings/dev.py
@@ -6,8 +6,4 @@ SECURE_SSL_REDIRECT = False
 
 DEBUG = True
 # workaround for django all auth
-# Toggle sandbox mode (when running in DEBUG mode)
-SENDGRID_SANDBOX_MODE_IN_DEBUG = True
-
-# echo to stdout or any other file-like object that is passed to the backend via the stream kwarg.
-SENDGRID_ECHO_TO_STDOUT = True
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
