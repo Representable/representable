0d08b3fd00d44ccb669922ff2df653411f225fa0
Lauren Johnston <laurenjohnston97@gmail.com>
2020-05-08 00:17:18 -0700

    Add travis default secret key

--- representable/settings/base.py
+++ representable/settings/base.py
@@ -39,7 +39,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howtodeployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["SECRET_KEY"]
+if os.getenv("BUILD_ON_TRAVIS", None):
+    SECRET_KEY = "TravisTestKey"
+    DEBUG = False
+    TEMPLATE_DEBUG = True
+else:
+    SECRET_KEY = os.environ["SECRET_KEY"]
 
 
 AUTHENTICATION_BACKENDS = (
