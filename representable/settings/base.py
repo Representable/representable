bac5f46d8130aadbcf534070642ee1abefe541b5
Lauren Johnston <laurenjohnston97@gmail.com>
2020-05-08 21:33:41 -0700

    Update travis.yml

--- representable/settings/base.py
+++ representable/settings/base.py
@@ -41,7 +41,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 if "TRAVIS" in os.environ:
     SECRET_KEY = "TravisTestKey"
-    DEBUG = False
     TEMPLATE_DEBUG = True
 else:
     SECRET_KEY = os.environ["SECRET_KEY"]
