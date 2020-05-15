f3f6d6386725ec5046f1665b0a4710bc11e6fcac
Kyle Barnes <ktbarnes@princeton.edu>
2020-05-15 20:47:32 +1000

    add states + dropdown :footprints:

--- main/static/main/js/main.js
+++ main/static/main/js/main.js
@@ -19,10 +19,85 @@
  */
 let jQuery = window.jQuery
 let states = ['mi', 'tx', 'va']
+let statesDatabase = {
+  'AL': 'Alabama',
+  'AK': 'Alaska',
+  'AS': 'American Samoa',
+  'AZ': 'Arizona',
+  'AR': 'Arkansas',
+  'CA': 'California',
+  'CO': 'Colorado',
+  'CT': 'Connecticut',
+  'DE': 'Delaware',
+  'DC': 'District Of Columbia',
+  'FM': 'Federated States Of Micronesia',
+  'FL': 'Florida',
+  'GA': 'Georgia',
+  'GU': 'Guam',
+  'HI': 'Hawaii',
+  'ID': 'Idaho',
+  'IL': 'Illinois',
+  'IN': 'Indiana',
+  'IA': 'Iowa',
+  'KS': 'Kansas',
+  'KY': 'Kentucky',
+  'LA': 'Louisiana',
+  'ME': 'Maine',
+  'MH': 'Marshall Islands',
+  'MD': 'Maryland',
+  'MA': 'Massachusetts',
+  'MI': 'Michigan',
+  'MN': 'Minnesota',
+  'MS': 'Mississippi',
+  'MO': 'Missouri',
+  'MT': 'Montana',
+  'NE': 'Nebraska',
+  'NV': 'Nevada',
+  'NH': 'New Hampshire',
+  'NJ': 'New Jersey',
+  'NM': 'New Mexico',
+  'NY': 'New York',
+  'NC': 'North Carolina',
+  'ND': 'North Dakota',
+  'MP': 'Northern Mariana Islands',
+  'OH': 'Ohio',
+  'OK': 'Oklahoma',
+  'OR': 'Oregon',
+  'PW': 'Palau',
+  'PA': 'Pennsylvania',
+  'PR': 'Puerto Rico',
+  'RI': 'Rhode Island',
+  'SC': 'South Carolina',
+  'SD': 'South Dakota',
+  'TN': 'Tennessee',
+  'TX': 'Texas',
+  'UT': 'Utah',
+  'VT': 'Vermont',
+  'VI': 'Virgin Islands',
+  'VA': 'Virginia',
+  'WA': 'Washington',
+  'WV': 'West Virginia',
+  'WI': 'Wisconsin',
+  'WY': 'Wyoming'
+}
 
 jQuery(document).ready(function () {
   setUpUSAMap()
+  populateStateSelectionDropdown()
 })
+
+let populateStateSelectionDropdown = function() {
+  let selectElement = document.getElementById('stateSelectionDropdown')
+  for (x in states) {
+    let stateName = statesDatabase[states[x].toUpperCase()]
+    let newOption = document.createElement('option')
+    newOption.value = states[x]
+    newOption.label = stateName
+    newOption.innerHTML = stateName
+    selectElement.appendChild(newOption)
+  }
+}
+
 
 let setUpUSAMap = function () {
   colors = {}
