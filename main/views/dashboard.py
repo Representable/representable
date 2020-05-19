874e2e8a318211aba65c7407305a7ec265ed043d
Lauren Johnston <laurenjohnston97@gmail.com>
2020-05-19 15:10:07 -0700

    Update permissions

--- main/views/dashboard.py
+++ main/views/dashboard.py
@@ -85,11 +85,26 @@ class OrgAdminRequiredMixin(UserPassesTestMixin):
 
 class OrgModRequiredMixin(UserPassesTestMixin):
     """
+    Checks if the user has at least moderator permissions for the given organization (checked through the pk field)
+    """
+
+    def test_func(self):
+        if self.request.user.is_org_moderator(
+            self.kwargs["pk"]
+        ) or self.request.user.is_org_admin(self.kwargs["pk"]):
+            return True
+
+
+# ******************************************************************************#
+
+
+class OrgMemberRequiredMixin(UserPassesTestMixin):
+    """
     Checks if the user has moderator permissions for the given organization (checked through the pk field)
     """
 
     def test_func(self):
-        return self.request.user.is_org_moderator(self.kwargs["pk"])
+        return self.request.user.is_member(self.kwargs["pk"])
 
 
 # ******************************************************************************#
@@ -184,7 +199,7 @@ class EditOrg(LoginRequiredMixin, OrgAdminRequiredMixin, UpdateView):
 # ******************************************************************************#
 
 
-class HomeOrg(LoginRequiredMixin, DetailView):
+class HomeOrg(LoginRequiredMixin, OrgMemberRequiredMixin, DetailView):
     """
     The admin home page for an organization within the dashboard
     """
@@ -400,10 +415,9 @@ class ReviewOrg(LoginRequiredMixin, OrgModRequiredMixin, TemplateView):
         return render(request, self.template_name, context)
 
 
-class CampaignHome(LoginRequiredMixin, DetailView):
+class CampaignHome(LoginRequiredMixin, OrgMemberRequiredMixin, DetailView):
     """
-    BETA view: the campaign home
-    Note: url rewrites currently not working with the given pk_url_kwarg.
+    The main campaign view
     """
 
     template_name = "main/dashboard/campaigns/index.html"
@@ -430,46 +444,9 @@ class CampaignHome(LoginRequiredMixin, DetailView):
         return context
 
 
-class CampaignList(LoginRequiredMixin, TemplateView):
+class CreateCampaign(LoginRequiredMixin, OrgAdminRequiredMixin, CreateView):
     """
-    BETA view: the dashboard list of campaigns
-    Note: url rewrites currently not working with the given pk_url_kwarg.
-    """
-
-    template_name = "main/dashboard/campaigns/list.html"
-    # model = Campaign
-    # #campaigns = Campaign.objects.all()
-    # pk_url_kwarg = 'cam_pk'
-    # #
-    # def get_queryset(self):
-    #    org = get_object_or_404(Organization, pk=self.kwargs["pk"])
-    #    return super(CampaignList, self).get_queryset().filter(organization=org)
-    # def get_queryset(self):
-    #     print(self.kwargs)
-    #     org = get_object_or_404(Organization, pk=self.kwargs["pk"])
-    #     return Campaign.objects.all()
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["is_org_admin"] = self.request.user.is_org_admin(
-            self.kwargs["pk"]
-        )
-        context["is_org_moderator"] = self.request.user.is_org_moderator(
-            self.kwargs["pk"]
-        )
-        context["org"] = get_object_or_404(Organization, pk=self.kwargs["pk"])
-        context[
-            "campaigns"
-        ] = (
-            Campaign.objects.all()
-        )  # filter(organization__id=self.kwargs["pk"])
-
-        return context
-
-
-class CreateCampaign(LoginRequiredMixin, CreateView):
-    """
-    BETA view: the view for the form to create a campaign
+    The view for the form to create a campaign
     """
 
     template_name = "main/dashboard/campaigns/create.html"
@@ -497,7 +474,7 @@ class CreateCampaign(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
 
-class UpdateCampaign(LoginRequiredMixin, UpdateView):
+class UpdateCampaign(LoginRequiredMixin, OrgAdminRequiredMixin, UpdateView):
     """
     BETA view: the view for the form to update campaign details
     """
