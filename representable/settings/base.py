666930caaa90b0a241ac5100e8e7999a71c88b41
Lauren Johnston <laurenjohnston97@gmail.com>
2020-05-20 13:34:58 -0700

    Update base.py

--- representable/settings/base.py
+++ representable/settings/base.py
@@ -229,3 +229,5 @@ elif DATABASES["default"]["ENGINE"] == "django.db.backends.sqlite3":
     DATABASES["default"][
         "ENGINE"
     ] = "django.contrib.gis.db.backends.spatialite"
+
+ACCOUNT_EMAIL_REQUIRED = True
