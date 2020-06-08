cfeaa04e993d637687449d68bc0598243cd49daf
Lauren Johnston <laurenjohnston97@gmail.com>
2020-06-08 16:59:45 -0700

    Update serializers.py to remove slug field

--- api/serializers.py
+++ api/serializers.py
@@ -5,4 +5,4 @@ from main.models import Organization
 class OrganizationSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Organization
-        fields = ("name", "description", "ext_link", "states", "slug")
+        fields = ("name", "description", "ext_link", "states")
