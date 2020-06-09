f717248269313aba74937afe00ab917b4ed1167e
Lauren Johnston <laurenjohnston97@gmail.com>
2020-06-08 22:52:37 -0700

    Add package.json, id field

--- api/serializers.py
+++ api/serializers.py
@@ -5,5 +5,5 @@ from main.models import Organization
 class OrganizationSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Organization
-        fields = ("name", "description", "ext_link", "states")
+        fields = ("id", "name", "description", "ext_link", "states")
         lookup_field = "slug"
