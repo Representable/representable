75def966ea687eea44227b6d90119c47bbe36751
Lauren Johnston <laurenjohnston97@gmail.com>
2020-06-11 14:44:05 -0700

    Removes tag model
    Removes tag model and artifacts on form page

--- main/choices.py
+++ main/choices.py
@@ -17,47 +17,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
-RACE_CHOICES = (
-    ("white", "White"),
-    ("black", "Black or African American"),
-    ("native", "American Indian or Alaska Native"),
-    ("pacific_islander", "Native Hawaiian or Other Pacific Islander"),
-    (
-        "Asian",
-        (
-            ("indian", "Asian Indian"),
-            ("chinese", "Chinese"),
-            ("filipino", "Filipino"),
-            ("japanese", "Japanese"),
-            ("korean", "Korean"),
-            ("vietnamese", "Vietnamese"),
-            ("other_asian", "Other Asian"),
-        ),
-    ),
-    ("other", "Other"),
-)
-RELIGION_CHOICES = (
-    ("christianity", "Christianity"),
-    ("buddhism", "Buddhism"),
-    ("islam", "Islam"),
-    ("judaism", "Judaism"),
-    ("hinduism", "Hinduism"),
-    ("other", "Other"),
-)
-
-# https://www.census.gov/programs-surveys/aces/information/iccl.html
-INDUSTRY_CHOICES = (
-    ("agriculture", "Agriculture"),
-    ("forestry", "Forestry"),
-    ("fishing", "Fishing"),
-    ("mining", "Mining"),
-    ("utilities", "Utilities (Power, Natural Gas)"),
-    ("construction", "Construction"),
-    ("manufacturing", "Manufacturing"),
-    ("professional", "Professional (Scientific/Technical) Services"),
-    ("arts", "Arts"),
-    ("other", "Other"),
-)
 
 STATES = (
     ("AL", "Alabama"),
