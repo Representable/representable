f0756809f0ae984f1bca893af971d450f911c50b
Lauren Johnston <laurenjohnston97@gmail.com>
2020-06-11 19:22:09 -0700

    working dashboard

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,47 +1,10 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
-// Adapted from: https://www.valentinog.com/blog/drf/
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: [],
-      loaded: false,
-      placeholder: "Loading",
-    };
-  }
+import Dashboard from "./Dashboard";
 
-  componentDidMount() {
-    fetch("api/organizations")
-      .then((response) => {
-        if (response.status > 400) {
-          return this.setState(() => {
-            return { placeholder: "Something went wrong!" };
-          });
-        }
-        return response.json();
-      })
-      .then((data) => {
-        this.setState(() => {
-          return {
-            data,
-            loaded: true,
-          };
-        });
-      });
-  }
-
-  render() {
-    return (
-      <ul>
-        {this.state.data.map((organization) => {
-          return <li key={organization.id}>{organization.name}</li>;
-        })}
-      </ul>
-    );
-  }
+function App() {
+  return <Dashboard></Dashboard>;
 }
-
 export default App;
 
 const container = document.getElementById("app");
