662c8a656210597f6415488721deea3de21f41aa
Lauren Johnston <laurenjohnston97@gmail.com>
2020-06-14 14:06:08 -0700

    Add export functionality

--- main/admin.py
+++ main/admin.py
@@ -19,6 +19,54 @@
 #
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+from main.models import CommunityEntry
 from .models import User
 
 admin.site.register(User, UserAdmin)
+
+
+class CommunityResource(resources.ModelResource):
+    class Meta:
+        model = CommunityEntry
+        fields = (
+            "id",
+            "user_name",
+            "entry_name",
+            "cultural_interests",
+            "economic_interests",
+            "comm_activities",
+            "user_polygon",
+            "census_blocks_polygon",
+            "created_at",
+        )
+        export_order = (
+            "id",
+            "user_name",
+            "entry_name",
+            "cultural_interests",
+            "economic_interests",
+            "comm_activities",
+            "created_at",
+            "user_polygon",
+            "census_blocks_polygon",
+        )
+
+
+class CommunityAdmin(ImportExportModelAdmin):
+    list_display = (
+        "id",
+        "user_name",
+        "entry_name",
+        "organization",
+        "campaign",
+    )
+    list_filter = (
+        "campaign",
+        "organization",
+    )
+    resource_class = CommunityResource
+
+
+admin.site.register(CommunityEntry, CommunityAdmin)
