12154c98c845d42f415a611d2c0ca764b16df91b
Lauren Johnston <laurenjohnston97@gmail.com>
2020-06-16 10:38:08 -0700

    Add docasaurus style guide

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -3,6 +3,6 @@ module.exports = {
     "Getting Started": ["setup", "doc1", "doc2", "doc3"],
     "Django Project": ["django1", "django2"],
     "React Visualizations App": ["viz1"],
-    "Docs Editing Guide": ["docasaurus1"],
+    "Docs Editing Guide": ["docasaurus1", "docasaurus2"],
   },
 };
