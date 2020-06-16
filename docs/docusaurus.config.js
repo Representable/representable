f8def20bc6649b911598bc020b0e300cadf7e440
Lauren Johnston <laurenjohnston97@gmail.com>
2020-06-15 22:00:11 -0700

    Add docs and viz app to main codebase

---
+++ docs/docusaurus.config.js
@@ -0,0 +1,98 @@
+module.exports = {
+  title: "Representable.org Docs",
+  tagline: "Getting started with Representable.org",
+  url: "https://representable-docs.netlify.app",
+  baseUrl: "/",
+  favicon: "img/favicon.ico",
+  organizationName: "representable", // Usually your GitHub org/user name.
+  projectName: "docs", // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: "Representable",
+      logo: {
+        alt: "My Site Logo",
+        src: "img/logo.svg",
+      },
+      links: [
+        {
+          to: "docs/",
+          activeBasePath: "docs",
+          label: "Docs",
+          position: "left",
+        },
+        { to: "blog/", label: "Blog", position: "left" },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Docs",
+          items: [
+            {
+              label: "Style Guide",
+              to: "docs/",
+            },
+            {
+              label: "Second Doc",
+              to: "docs/doc2/",
+            },
+          ],
+        },
+        {
+          title: "Community",
+          items: [
+            {
+              label: "Stack Overflow",
+              href: "https://stackoverflow.com/questions/tagged/docusaurus",
+            },
+            {
+              label: "Discord",
+              href: "https://discordapp.com/invite/docusaurus",
+            },
+            {
+              label: "Twitter",
+              href: "https://twitter.com/docusaurus",
+            },
+          ],
+        },
+        {
+          title: "More",
+          items: [
+            {
+              label: "Blog",
+              to: "blog",
+            },
+            {
+              label: "GitHub",
+              href: "https://github.com/representable/docs",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Representable, Inc. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        docs: {
+          // It is recommended to set document id as docs home page (`docs/` path).
+          homePageId: "doc1",
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          editUrl: "https://github.com/Representable/docs/edit/master/",
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl: "https://github.com/Representable/docs/edit/master/",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      },
+    ],
+  ],
+};
--- docs/fixtures.md
+++
@@ -1,9 +0,0 @@
-## How to populate db:
-
-### run command:
-
-_**python manage.py loaddata FixtureUpdated.json**_
-
-where fixtureTest.json is saved inside path: _representable/main/fixtures/FixtureUpdated.json_
-
-https://docs.djangoproject.com/en/2.2/howto/initial-data/
