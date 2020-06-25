39196ac822096f9bf5960768b9f8669f09587b83
Theodor Marcu <marcu.theodor@gmail.com>
2020-06-25 12:32:05 -0400

    updated email settings, included custom debug adapter

--- representable/settings/dev.py
+++ representable/settings/dev.py
@@ -6,4 +6,10 @@ SECURE_SSL_REDIRECT = False
 
 DEBUG = True
 
+# Dev Email Settings - Print to Console
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+# Use Custom Debugging Email Adapter that prints a simple text message.
+# See docs for context: https://django-allauth.readthedocs.io/en/latest/advanced.html#sending-email
+ACCOUNT_ADAPTER = "main.users.adapter.DebugAdapter"
+
 MIXPANEL_TOKEN = "ce31fc3e8e15a16619bb3672f9c25407"
--- representable/settings/prod.py
+++ representable/settings/prod.py
@@ -10,4 +10,16 @@ ALLOWED_HOSTS = ["www.representable.org", "representable.org"]
 
 DEBUG = False
 
+# Production Email Settings
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+# Uses default Account Adapter.
+# SMTP Server Settings
+SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY")
+EMAIL_HOST = "smtp.sendgrid.net"
+EMAIL_HOST_USER = "apikey"
+EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+
 MIXPANEL_TOKEN = "d0cc0bfda7587bccdb286d10deeac993"
