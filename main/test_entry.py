50745618ce7a267b363bab31a2b390d00b14948f
Edward Tian <edwardtian2000@gmail.com>
2020-06-30 10:32:42 -0400

    valid_form and draft of other tests

--- main/test_entry.py
+++ main/test_entry.py
@@ -32,7 +32,6 @@ class EntryTest(StaticLiveServerTestCase):
         self.user.delete()
 
     def test_template(self):
-        # self.assertTemplateUsed(self.response, '/main/templates/main/entry.html')
         # Testing Page Active
         self.assertTrue(self.response.context["user"].is_active)
         self.assertEqual(self.response.status_code, 200)
@@ -41,10 +40,42 @@ class EntryTest(StaticLiveServerTestCase):
     #     print("Test create entry")
     #     entry = CommunityEntry.objects.create(user=self.user)
     #     self.assertFalse(entry.is_valid())
-    #     #self.assertIsNotNone(entry.get_absolute_url())
-    #     #CommunityForm.save(form)
+    #     self.assertIsNotNone(entry.get_absolute_url())
+    #     CommunityForm.save(form)
 
     def test_empty_form(self):
-        data = {}
+        # self.assertIsInstance(self.response.context['form'], CommunityForm)
+        data = {"comm_activities": ""}
         form = CommunityForm(data=data)
+        # self.assertEqual(
+        #     form.errors['text'],
+        #     ["User polygon missing. Please draw your community."]
+        # )
         self.assertFalse(CommunityForm.is_valid(form))
+
+    # Reference: www.obeythetestinggoat.com/book/chapter_simple_form.html
+    def test_valid_form(self):
+
+        data = {
+            "entry_ID": "9330c85d-7d52-485f-ab18-78e336f3fd49",
+            "user_id": 64,
+        }
+        form = CommunityForm(data=data)
+        form.save()
+        self.assertTrue(CommunityForm.is_valid(form))
+
+    # def post_invalid_input(self):
+    #     list_ = CommunityEntry.objects.create()
+    #     return self.client.post(
+    #         f'/lists/{list_.id}/',
+    #         data={'text': ''}
+    #     )
+    #
+    # def test_for_invalid_input_nothing_saved_to_db(self):
+    #     self.post_invalid_input()
+    #     self.assertEqual(CommunityEntry.objects.count(), 0)
+
+    # def test_for_invalid_input_renders_list_template(self):
+    #     self.response = self.post_invalid_input()
+    #     self.assertEqual(self.response.status_code, 200)
+    #     self.assertTemplateUsed(self.response, 'entry.html')
