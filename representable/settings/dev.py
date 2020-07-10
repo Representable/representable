ef915f87929d597332706a5c4e3bb30502c9b040
Lauren Johnston <laurenjohnston97@gmail.com>
2020-07-10 10:12:19 -0700

    Update dev.py

--- representable/settings/dev.py
+++ representable/settings/dev.py
@@ -13,10 +13,4 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 # See docs for context: https://django-allauth.readthedocs.io/en/latest/advanced.html#sending-email
 ACCOUNT_ADAPTER = "main.users.adapter.DebugAdapter"
 
-# Delete before pushing
-# Production Email Settings
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-# Uses default Account Adapter.
-# SMTP Server Settings
-
 MIXPANEL_TOKEN = "ce31fc3e8e15a16619bb3672f9c25407"
