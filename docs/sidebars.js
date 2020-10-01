1852ad63a9a6b2f4b13fa6655cd701ee9f30c0f2
Kyle Barnes <ktbarnes@princeton.edu>
2020-10-01 11:07:57 -0400

    docs for setup + accessibility

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -2,12 +2,14 @@ module.exports = {
   someSidebar: {
     "Getting Started": [
       "setup",
+      "setup2",
       "postgres",
       "geodjango",
       "doc1",
       "server",
       "doc2",
     ],
+    "Accessibility": ["access1"],
     "Django Project": ["django1", "django2"],
     "React Visualizations App": ["viz1"],
     "Vanilla JS Visualizations": ["vanillaviz1", "vanillaviz2"],
