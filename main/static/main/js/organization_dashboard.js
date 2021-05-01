58bbac950a66479dd07164331628a1aee0fa8201
chuk413 <32914259+chuk413@users.noreply.github.com>
2021-05-01 15:47:24 -0700

    Redesigns the Partner Welcome Page
    **changes**
    - Redesigns the partner welcome page as featured on the figma doc

---
+++ main/static/main/js/organization_dashboard.js
@@ -0,0 +1,7 @@
+$("#partner-welcome-btn").on("click", function() {
+    $("#partner-welcome-modal").modal();
+});
+
+$("#partner-welcome-btn-mobile").on("click", function() {
+    $("#partner-welcome-modal").modal();
+});
