307d983d1fb3261c32c034d80fa06347af9d316c
Jason Yuan <jasony123123@gmail.com>
2021-05-25 18:14:22 -0500

    faster precompute

--- representable/staticfiles/script.py
+++ representable/staticfiles/script.py
@@ -10,29 +10,32 @@ for i in range(0, len(data['features'])):
 	unique_ids.add(data['features'][i]['properties']['GEOID'])
 assert len(data['features']) == len(unique_ids), 'GEOIDs are not unique'
 
-ftrs = dict()
-for i in range(0, len(data['features'])): 
+info = dict()
+
+for i in range(0, 25): 
 	my_geoid = data['features'][i]['properties']['GEOID']
 	my_ftr = Feature(geometry=data['features'][i]['geometry'])
-	ftrs[my_geoid] = my_ftr
- 
-progress = len(ftrs)*(len(ftrs)-1)
-info = dict()
-for my_geoid, my_ftr in ftrs.items():
-	adjlist = []
-	for other_geoid, other_ftr in ftrs.items():
-		if my_geoid != other_geoid:
-			un = union(FeatureCollection([my_ftr, other_ftr]))
+	info[my_geoid] = {
+		'adj_geoids': [],
+		'feature_obj': my_ftr,
+	} 
+
+progress = len(info)*(len(info)-1)/2
+for my_geoid in info.keys():
+	for other_geoid in info.keys():
+		if my_geoid < other_geoid:
+			un = union(FeatureCollection([
+				info[my_geoid]['feature_obj'], 
+				info[other_geoid]['feature_obj']
+			]))
 			if un['geometry']['type'] == 'Polygon':
-				adjlist.append(other_geoid)
+				info[my_geoid]['adj_geoids'].append(other_geoid)
+				info[other_geoid]['adj_geoids'].append(my_geoid)
+
 			print(progress, my_geoid, other_geoid, un['geometry']['type'])
 			progress -= 1
-	info[my_geoid] = {
-		'adj_geoids': adjlist,
-		'feature_obj': my_ftr,
-	}
 
-with open('ndbg-precompute.json', 'w') as outfile:
+with open('fast-ndbg-precompute.json', 'w') as outfile:
 	json.dump(info, outfile)
 
 # print(json.dumps(un, indent=2, sort_keys=True))
