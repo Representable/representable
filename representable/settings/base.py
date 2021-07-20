c53718bda26bdf878982859ce1a86f903927b65f
aceaton <aceaton@princeton.edu>
2021-07-20 12:25:33 -0400

    staticfiles issue

--- representable/settings/base.py
+++ representable/settings/base.py
@@ -202,16 +202,18 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    # os.path.join(BASE_DIR, 'static'),
+    # os.path.join(os.path.normpath(BASE_DIR), 'static'),
     )
 
 # Mapbox API Key
